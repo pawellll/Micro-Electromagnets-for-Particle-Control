62317ffc4c677f96d92dd3337dc4c96aaf1016ca
pawellll <pmpeksa@gmail.com>
2014-07-11 09:53:12 +0200

    kontr.java = controller który wyswietla bitmape

--- GDPC/src/GD/kontr.java
+++ GDPC/src/GD/kontr.java
@@ -1,29 +1,35 @@
 package GD;
 
 import GUI.GUIAction;
-import Platform.Main;
+import Platform.CrossBitmap;
+import Supp.Comm;
+import Supp.Dim;
 import Supp.DrawingInterface;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-public class kontr extends Controller
-{
+public class kontr extends Controller {
 
-	@Override
-	public void onPaint(DrawingInterface g, int width, int height)
-	{
-		g.gDrawRectangle(50, 50, 100, 100);
-	}
+    CrossBitmap image;
 
-	@Override
-	public void onGUIAction(GUIAction e)
-	{
-	}
+    kontr(File file) throws IOException {
+        image = new CrossBitmap(ImageIO.read(file));
+    }
 
-	@Override
-	public void onSetCurrent()
-	{
-				gui.clearComponents();
-				Main.main.canvas.paint();
-		
-	}
-	
+    @Override
+    public void onPaint(DrawingInterface g, int width, int height) {
+        g.gDrawRectangle(50, 50, 100, 100);
+        g.gStretchBitmap(0, 0, width, Dim.Y(26.5), image, Comm.STRETCH_H_PROP);
+    }
+
+    @Override
+    public void onGUIAction(GUIAction e) {
+    }
+
+    @Override
+    public void onSetCurrent() {
+        gui.clearComponents();
+    }
+
 }
