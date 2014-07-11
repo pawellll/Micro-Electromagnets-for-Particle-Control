ac0b952cc3994d5a22285505e0670af975c19134
dareek <djzielinski@interia.pl>
2014-07-11 13:42:45 +0200

    Poprawiony GUIImage

--- GDPC/src/GUI/GUIImage.java
+++ GDPC/src/GUI/GUIImage.java
@@ -38,13 +38,13 @@ public class GUIImage extends GUIComponent
     
 		if (stretchFlag == Comm.STRETCH_H_PROP)
 		{
-			nh = (int)((double)nh*(double)w/nw);
+			nh = Dim.dblY((double)nh*(double)Dim.X(w)/nw);
 			nw = w;
 		}
 		else
 		if (stretchFlag == Comm.STRETCH_V_PROP)
 		{
-			nw = (int)((double)nw*(double)h/nh);
+			nw = Dim.dblX((double)nw*(double)Dim.Y(h)/nh);
 			nh = h;
 		}
 		else
@@ -86,6 +86,8 @@ public class GUIImage extends GUIComponent
 	@Override
 	public void onPaint(DrawingInterface g, int width, int height)
 	{
+		setStretchFlag(stretch);
+		
 		g.gStretchBitmap(getX(), getY(), Dim.X(w, minX), Dim.Y(h, minY), img, Comm.STRETCH_HV);
 	
 		if (selectable && selectStarted)
@@ -96,7 +98,7 @@ public class GUIImage extends GUIComponent
 			g.gDrawRectangle((int)Math.min(startX, endX)+1, (int)Math.min(startY, endY)+1, (int)Math.abs(endX-startX)-2, (int)(Math.abs(endY-startY))-2);
 			
 			g.gSetColor(Colors.GREEN, 128);
-			g.gFillRectangle((int)Math.min(startX, endX)+2, (int)Math.min(startY, endY)+2, (int)Math.abs(endX-startX)-4, (int)(Math.abs(endY-startY))-4);
+			g.gFillRectangle((int)Math.min(startX, endX)+2, (int)Math.min(startY, endY)+2, (int)Math.abs(endX-startX)-3, (int)(Math.abs(endY-startY))-3);
 		
 		}
 	}
@@ -133,10 +135,12 @@ public class GUIImage extends GUIComponent
 	@Override
 	public void onMouseUp(double x, double y)
 	{
-		if (selectable && selectStarted)
+		if (selectable && selectStarted && endX!=0 && endY!=0)
 		{
 			Main.main.mainCrtl.currentCtrl.onGUIAction(new GUIAction(id, GUIAction.IMG_SELECTED, new DoubleArea(startX, startY, endX, endY)));	
 		  selectStarted = false;
+			endX = 0;
+			endY = 0;
 			Main.main.canvas.paint();
 		}
 	}
